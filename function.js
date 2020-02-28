253d1eaf5fe58432842a65315eb03fe129dc510f
root <root@ip-172-31-36-188.us-west-2.compute.internal>
2020-02-28 17:48:58 +0000

    :bug: Bug fixes in function

--- function.js
+++ function.js
@@ -1,21 +1,23 @@
 const AWS = require('aws-sdk');
 const fs = require('fs');
-const { aws_access_key, aws_secret_access_key } = require('config.js');
+const { aws_access_key, aws_secret_access_key } = require('./config.js');
 const s3 = new AWS.S3({
     accessKeyId: aws_access_key,
     secretAccessKey: aws_secret_access_key
 });
 
-function main(params) {
+function test(params) {
     const key = params.key;
-    const params = {
+    const bucketParams = {
         Bucket: 'mybucket-test-openwhisk',
         Key: key
     }
 
-    const rs = s3.getObject(params).createReadStream();
+    const rs = s3.getObject(bucketParams).createReadStream();
     console.log('Read Stream: ', rs);
     return {
         msg: 'Hello World'
     }
 }
+
+exports.main = test
